92e3e1a871a4457da912f69a86ca1ad960a85460
Marcel Horvath <marcel.horvath@tul.cz>
2022-06-28 20:08:25 +0200

    almost done

--- pomodoro.js
+++ pomodoro.js
@@ -1,4 +1,3 @@
-//DOCUMENT
 const startButton =  document.querySelector("#start");
 const clearButton =  document.querySelector("#clear");
 const timerText = document.querySelector("#timerText");
@@ -8,67 +7,57 @@ const sliderPomodoro = document.querySelector("#pomodoroTimeSlider");
 const outputPomodoro = document.querySelector("#pomodoroTimeSliderValue");
 const sliderBreak = document.querySelector("#breakTimeSlider");
 const outputBreak = document.querySelector("#pomodoroBreakSliderValue");
-const buttons = document.querySelectorAll("button");
+const settingsButton =  document.querySelector("#settings")
+const sliders = document.querySelector(".slidersContainer");
+const sessionsContainer = document.querySelector(".sessions");
+
 const clickSound = new Audio("mouse-click.wav");
 const alarmSound = new Audio("alarm.wav")
-clickSound.volume = 0.3;
-//VARIABLES
-let time = 2700;
+clickSound.volume = 0.1;
+
+let time = 3;
 let timeValue = time;
 let timeInterval;
 let isCounting = false;
-console.log(buttons);
+let isBreak = false;
+let session = 1;
+let isCreated = false;
 
-//SLIDERS
-outputPomodoro.innerHTML = sliderPomodoro.value; // Display the default slider value
-// Update the current slider value (each time you drag the slider handle)
+outputPomodoro.innerHTML = sliderPomodoro.value;
 sliderPomodoro.oninput = function() {
     outputPomodoro.innerHTML = this.value;
 }
 
-outputBreak.innerHTML = sliderBreak.value; // Display the default slider value
-// Update the current slider value (each time you drag the slider handle)
+outputBreak.innerHTML = sliderBreak.value;
 sliderBreak.oninput = function() {
     outputBreak.innerHTML = this.value;
 }
 
-//LISTENERS
-startButton.addEventListener("click",startCountDown,false);
-clearButton.addEventListener("click",clearCountDown,false);
-pomodoroTime.addEventListener("click",setPomodoroTime,false);
-breakTime.addEventListener("click",setPomodoroTime2,false);
-for(const button of buttons){
-    button.addEventListener("click",playSound,false);
-}
-
-function playSound(){
+document.onclick = function playSound(){
     clickSound.load();
-    clickSound.play();
-}
-function setPomodoroTime(){
-    document.body.style.setProperty("--backgroundColor", "#f06865")
-    document.body.style.setProperty("--itemColor", "#F08080")
-    time = sliderPomodoro.value*60;
-    timeValue = time;
-    clearInterval(timeInterval);
-    timerText.textContent = "00:00"
-    startButton.textContent = "START"
-    isCounting = false;
-}
-function setPomodoroTime2(){
-    document.body.style.setProperty("--backgroundColor", "#3A78F0")
-    document.body.style.setProperty("--itemColor", "#4F9FF0")
-
-    time = sliderBreak.value*60;
-    timeValue = time;
-    clearInterval(timeInterval);
-    timerText.textContent = "00:00"
-    startButton.textContent = "START"
-    isCounting = false;
+    clickSound.play().then(r => console.log(r));
 }
 
-function startCountDown(){
-    if(!isCounting){
+settingsButton.onclick = function settingsPop() {
+    if (sliders.style.display === "none") {
+        sliders.style.display = "block";
+    } else {
+        sliders.style.display = "none";
+    }
+}
+
+pomodoroTime.onclick = function(){
+    setPomodoroTime("#f06865" ,"#F08080" ,sliderPomodoro.value)
+    isBreak = false;
+}
+
+breakTime.onclick = function(){
+    setPomodoroTime("#3A78F0", "#4F9FF0", sliderBreak.value)
+    isBreak = true;
+}
+
+startButton.onclick = function startCountDown(){
+    if(!isCounting && time > 0){
         timeInterval = setInterval(countdown, 1000);
         startButton.textContent = "PAUSE"
         isCounting = true;
@@ -80,15 +69,29 @@ function startCountDown(){
     }
 }
 
-function clearCountDown(){
-    timeValue = time;
-    clearInterval(timeInterval);
-    timerText.textContent = "00:00"
-    startButton.textContent = "START"
-    isCounting = false;
+clearButton.onclick = function clearCountDown(){
+    if (!isBreak){
+        setPomodoroTime("#f06865" ,"#F08080" ,sliderPomodoro.value)
+    }
+    else{
+        setPomodoroTime("#3A78F0", "#4F9FF0", sliderBreak.value)
+    }
+    while (sessionsContainer.firstChild) {
+        sessionsContainer.removeChild(sessionsContainer.lastChild);
+    }
+    session=1;
+    isCreated = false;
 }
-
 function countdown() {
+
+    if(!isBreak && isCreated === false){
+        let time= new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+        let sessionText = document.createTextNode("Session "+ session+".  Started at:" + time)
+        sessionsContainer.appendChild(sessionText);
+        sessionsContainer.appendChild(document.createElement("br"))
+        isCreated=true;
+    }
+
     const minutes = Math.floor(timeValue / 60);
     let seconds = timeValue % 60;
 
@@ -98,17 +101,32 @@ function countdown() {
     timeValue--;
 
     if (timeValue < 0) {
-        alarmSound.play();
-        clearInterval(timeInterval);
-    }
-}
-const settingsButton =  document.querySelector("#settings")
-settingsButton.onclick = function settingsPop() {
-    let x = document.querySelector(".slidersContainer");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
+        alarmSound.play().then(r => console.log(r));
+        if(!isBreak){
+            setPomodoroTime("#3A78F0", "#4F9FF0", sliderBreak.value)
+        }
+        else{
+            setPomodoroTime("#f06865" ,"#F08080" ,sliderPomodoro.value)
+        }
+        if(isBreak === false){
+            isBreak=true;
+            session++;
+        }
+        else if(isBreak === true){
+            isBreak=false;
+            isCreated=false;
+        }
+        timeInterval = setInterval(countdown, 1000);
     }
 }
 
+function setPomodoroTime(backgroundColor,itemColor, value){
+    document.body.style.setProperty("--backgroundColor", backgroundColor)
+    document.body.style.setProperty("--itemColor", itemColor)
+    time = value;
+    timeValue = time;
+    clearInterval(timeInterval);
+    timerText.textContent = "00:00"
+    startButton.textContent = "START"
+    isCounting = false;
+}
